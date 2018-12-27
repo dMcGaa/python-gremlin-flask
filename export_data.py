b110b8e41b4762436806b9b87718272bfdf7d9c7
David McGaa <mcgaa.david@gmail.com>
2018-12-27 09:21:46 -0600

    Export data, added directory to export data into

--- export_data.py
+++ export_data.py
@@ -6,6 +6,12 @@ def get_db():
 def get_person():
     gremlin_op = "g.V()"
     vertices_rep = ".hasLabel('person')"
+    gmn_pj = ".project('props', 'id','label')"
+    gmn_pj_props = ".by(valueMap())"
+    gmn_pj_id = ".by(id)"
+    gmn_pj_label = ".by(label)"
+    vertices_rep = ''.join([vertices_rep,gmn_pj, gmn_pj_props, gmn_pj_id, gmn_pj_label])
+
     gremlinCmd = ''.join([gremlin_op, vertices_rep])
     result_set = gremlinClient.submitAsync(gremlinCmd)
     future_persons = result_set.result() # a ResultSet
@@ -14,7 +20,7 @@ def get_person():
     return result
 
 def write_array(arr):
-    file = open("datafile.txt","w")
+    file = open("./data/datafile.txt","w")
     for i in arr:
         file.write(str(i))
         file.write("\n")
